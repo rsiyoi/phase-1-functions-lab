1b3a1c9b6de78b23a30856ea54236aaaee9cb3f3
Ruth Siyoi <ruth.siyoi@gmail.com>
2025-03-19 12:32:09 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const headquarters = 42;
+const feetPerBlock = 264;
+
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - headquarters);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return Math.abs(distanceFromHqInBlocks(pickupLocation) * feetPerBlock);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * feetPerBlock;
+}
+
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
